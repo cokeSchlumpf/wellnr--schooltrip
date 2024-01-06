bd9ad9a4047293cf78cca9ed500d696ddfc7dab6
Michael Wellner <michaelwellner@kpmg.com>
2024-01-06 20:47:37 +0100

    sc

--- src/main/java/com/wellnr/schooltrip/ui/components/EnterPaymentsDialog.java
+++ src/main/java/com/wellnr/schooltrip/ui/components/EnterPaymentsDialog.java
@@ -10,6 +10,7 @@ import com.wellnr.common.markup.Nothing;
 import com.wellnr.ddd.commands.MessageResult;
 import com.wellnr.schooltrip.core.application.commands.schooltrip.AddPaymentCommand;
 import com.wellnr.schooltrip.core.model.student.Student;
+import com.wellnr.schooltrip.core.model.student.questionaire.TShirtSelection;
 import com.wellnr.schooltrip.core.ports.i18n.SchoolTripMessages;
 import com.wellnr.schooltrip.infrastructure.ApplicationCommandRunner;
 import com.wellnr.schooltrip.ui.components.forms.ApplicationCommandForm;
@@ -74,12 +75,11 @@ public class EnterPaymentsDialog extends Dialog {
 
     private FormLayout getStudentSearchControls() {
         var formLayout = new FormLayout();
-        formLayout.setResponsiveSteps(new FormLayout.ResponsiveStep("0", 4));
+        formLayout.setResponsiveSteps(new FormLayout.ResponsiveStep("0", 5));
 
         var txtId = new ComboBox<Student>(i18n.id());
         txtId.setItems(students);
         txtId.setItemLabelGenerator(s -> s.getSchoolTripStudentId().map(Object::toString).orElse("-"));
-
 
         var txtSchoolClass = new ComboBox<Student>(i18n.schoolClass());
         txtSchoolClass.setItems(students);
@@ -93,13 +93,20 @@ public class EnterPaymentsDialog extends Dialog {
         txtLastName.setItems(students);
         txtLastName.setItemLabelGenerator(Student::getLastName);
 
-        formLayout.add(txtId, txtSchoolClass, txtFirstName, txtLastName);
+        var txtTShirt = new TextField(i18n.tripTShirt());
+        txtTShirt.setEnabled(false);
+
+        formLayout.add(txtId, txtSchoolClass, txtFirstName, txtLastName, txtTShirt);
 
         Procedure1<Student> valueChangeListener = student -> {
             txtId.setValue(student);
             txtSchoolClass.setValue(student);
             txtFirstName.setValue(student);
             txtLastName.setValue(student);
+            txtTShirt.setValue(i18n.tShirtSelection(
+                student.getQuestionnaire().map(q -> q.getTShirtSelection()).orElse(TShirtSelection.NONE),
+                i18n
+            ));
 
             if (this.command == null) {
                 this.addPaymentForm.setGetInitialCommand(() -> AddPaymentCommand.apply(student));
